4c954e43c45dc43acf64ec958061cbcabf22de43
A <halexhansen@gmail.com>
2022-10-27 10:03:17 -0400

    Removed debug readout, added in helpful_action pruning

--- planner/planner.py
+++ planner/planner.py
@@ -50,6 +50,11 @@ class Planner:
     def goal_diff(self, state):
         return (self.goal_state[0].difference(state)).union(self.goal_state[1].difference(state))
 
+    def action_from_name(self, name):
+        for action in self.parser.actions:
+            if action.name == name:
+                return action
+
     def creating_action(self, state, prev_action_layer):
         for action in self.parser.actions:
             if action.name in set(prev_action_layer):
@@ -85,23 +90,40 @@ class Planner:
                         else:
                             explored_states.add(next_state)
                             queue.insert(-1, (next_state, plan))
+        return None, None
 
     def enforced_hill_climbing_with_ff(self, init_state, actions):
         explored_states = set([init_state])
         queue = [(init_state, [])]
+        cur_state = init_state
         plan = []
         relaxed_plan, heuristic = self.ff(init_state, actions)
         while heuristic != 0:
-            pprint("Plan: {}\
-                    Heuristic: {}".format(plan, heuristic))
-            import ipdb; ipdb.set_trace()
             heur_funct = self.partial_ff_heuristic_comp(heuristic, actions)
-            next_state, bfs_plan = self.bfs(queue, explored_states, actions, heur_funct)
-            queue = [(next_state, [])]
-            explored_states = set([next_state])
+            helpful_actions = self.helpful_actions(relaxed_plan, cur_state)
+            # Try to use helpful actions as a first resort
+            if len(helpful_actions) > 0:
+                next_state, bfs_plan = self.bfs(queue.copy(), explored_states.copy(), helpful_actions, heur_funct)
+            # All actions if no helpful actions or nothing returned 
+            if len(helpful_actions) == 0 or next_state == None:
+                next_state, bfs_plan = self.bfs(queue.copy(), explored_states.copy(), actions, heur_funct)
+            cur_state = next_state
+            queue = [(cur_state, [])]
+            explored_states = set([cur_state])
             plan += bfs_plan
-            relaxed_plan, heuristic = self.ff(next_state, actions)
+            relaxed_plan, heuristic = self.ff(cur_state, actions)
         return plan
+
+    def helpful_actions(self, relaxed_plan, cur_state):
+        helpful_actions = []
+        for action_n in relaxed_plan[0]:
+            action = self.action_from_name(action_n)
+            p_precons = action.positive_preconditions
+            n_precons = action.negative_preconditions
+            if self.valid(cur_state, p_precons, n_precons):
+                if len(self.which_goals(action.add_effects)) > 0:
+                    helpful_actions.append(action)
+        return helpful_actions
 
     def partial_ff_heuristic_comp(self, heuristic, actions):
         def better_ff(state):
